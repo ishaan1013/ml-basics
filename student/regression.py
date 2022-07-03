742abca156d482245423c7eec2eed061d40dc1e4
ishaan1013 <ishaan1013@gmail.com>
2022-07-03 12:17:26 -0400

    train models

--- student/regression.py
+++ student/regression.py
@@ -16,12 +16,19 @@ X = np.array(data.drop([predict],1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-"""linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test, y_test)
+# best=0
+# for a in range(10000):
+#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+#     linear = linear_model.LinearRegression()
+#     linear.fit(x_train, y_train)
+#     accuracy = linear.score(x_test, y_test)
+#     # print(accuracy)
+#     if accuracy > best:
+#         best = accuracy
+#         with open("student/student.pickle","wb") as f:
+#             pickle.dump(linear,f)
 
-with open("student/student.pickle","wb") as f:
-    pickle.dump(linear,f)"""
+# print(best)
 
 pickle_in = open("student/student.pickle","rb")
 linear = pickle.load(pickle_in)
@@ -32,5 +39,5 @@ linear = pickle.load(pickle_in)
 
 predictions = linear.predict(x_test)
 
-for i, v in enumerate(predictions):
-    print(v, x_test[i], y_test[i])
+# for i, v in enumerate(predictions):
+#     print(v, x_test[i], y_test[i])
