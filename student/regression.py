f0d51d2d74e9ae7fbec0a7d824c32c7182cb2e23
ishaan1013 <ishaan1013@gmail.com>
2022-07-03 12:13:17 -0400

    save models

--- student/regression.py
+++ student/regression.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
+# from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
 
 data=pd.read_csv("student/student-mat.csv",sep=";")
 data=data[["G1","G2","G3","studytime","goout","Dalc","Walc","failures","absences"]]
@@ -13,9 +16,15 @@ X = np.array(data.drop([predict],1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+"""linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
 accuracy = linear.score(x_test, y_test)
+
+with open("student/student.pickle","wb") as f:
+    pickle.dump(linear,f)"""
+
+pickle_in = open("student/student.pickle","rb")
+linear = pickle.load(pickle_in)
 
 # print("accuracy: ", accuracy)
 # print("coefficient: ", linear.coef_)
